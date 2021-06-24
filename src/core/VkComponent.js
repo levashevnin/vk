fee779d9a80274e09eeb6f1334750eadfb3d51d3
levashevnin <levashevnin@mail.ru>
2021-06-24 19:06:36 +0300

    Finish base framework functional

---
+++ src/core/VkComponent.js
@@ -0,0 +1,20 @@
+import {DomListener} from "@core/DomListener";
+
+export class VkComponent extends DomListener{
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+        this.name = options.name || ''
+    }
+    // Вовзвращает шаблон компонента
+    toHTML(){
+        return ''
+    }
+
+    init(){
+        this.initDOMListeners()
+    }
+
+    destroy(){
+        this.removeDOMListeners()
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import './scss/index.scss'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import './simplebar'
+import {Vk} from "@/components/vk/Vk";
+import {Stickers} from "@/components/stickers/Stickers";
+import {Input} from "@/components/input/Input";
 
-console.log('HUI')
+
+
+const vk = new Vk('#app', {
+    components: [Stickers, Input]
+})
+
+vk.render()
